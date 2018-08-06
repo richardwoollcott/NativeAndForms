76b667b7fe5755ddd8d04b4f5ef3934fcb820a6d
Richard Woollcott <richardwoollcott@hotmail.com>
2018-08-06 11:56:42 +0100

    Using shared styles

---
+++ NativeAndForms/Views/SharedStyles.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace NativeAndForms.Views
+{
+    public partial class SharedStyles : ResourceDictionary
+    {
+        public SharedStyles()
+        {
+            InitializeComponent();
+        }
+    }
+}
