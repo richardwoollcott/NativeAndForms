8d434cec713a489222f7cc43c811d8919f6eeba9
Richard Woollcott <richardwoollcott@hotmail.com>
2018-08-06 09:32:50 +0100

    * storage.ide: Added custom renderer for an Entry control to demo   that in all projects
    * FormsShared.shproj:
    * EntryRenderer_iOS.cs:
    * FormsShared.projitems:
    * EntryRenderer_Droid.cs:
    * MyEntry.cs:
    * UserPrefs.xml:
    * project.assets.json:
    * storage.ide-shm:
    * storage.ide-wal:
    * NativeAndForms.csproj.nuget.cache:
    * NativeAndForms.csproj.nuget.g.props:
    * NativeAndForms.csproj.nuget.g.targets:
    * NativeAndForms.dll:
    * NativeAndForms.dll:
    * NativeAndForms.pdb:
    * NativeAndForms.pdb:
    * NativeAndForms.deps.json:
    * NativeAndForms.csproj.FileListAbsolute.txt:
    * NativeAndForms.csproj.CoreCompileInputs.cache:
    * NativeAndForms.csprojResolveAssemblyReference.cache:

---
+++ FormsShared/EntryRenderer_Droid.cs
@@ -0,0 +1,29 @@
+﻿#if __ANDROID__
+using System;
+using Android.Content;
+using FormsShared;
+using NativeAndForms.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace FormsShared
+{
+    class MyEntryRenderer : EntryRenderer
+    {
+        public MyEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(global::Android.Graphics.Color.LightGreen);
+            }
+        }
+    }
+}
+#endif
---
+++ FormsShared/EntryRenderer_iOS.cs
@@ -0,0 +1,26 @@
+﻿#if __IOS__
+using System;
+using FormsShared;
+using NativeAndForms.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer (typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace FormsShared
+{
+    public class MyEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged (ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged (e);
+
+            if (Control != null) {
+                // do whatever you want to the UITextField here!
+                Control.BackgroundColor = UIColor.FromRGB (204, 153, 255);
+                Control.BorderStyle = UITextBorderStyle.Line;
+            }
+        }
+    }
+}
+#endif
